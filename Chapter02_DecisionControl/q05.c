864476c03c1e476808708d26ec8fa82b5e08410b
Adarsh-AR50 <adarshrajuskar1@gmail.com>
2026-03-22 18:34:31 +0530

    Solved Chapter 2 Question 5 - Reverse number and check palindrome

--- Chapter02_DecisionControl/q05.c
+++ Chapter02_DecisionControl/q05.c
@@ -1,14 +1,14 @@
 /*
-A five-digit number is entered through the keyboard.
+A five-digit is entered through the keyboard.
 Write a program to obtain the reversed number 
-And to determine whether the original and reversed numbers are equal or not
+And to detemine whether the original and reversed numbers are equal or not
 */
 
 #include <stdio.h>
 
 int main()
 {
-    int 
+    int num, temp, rem, sum = 0;
 
     printf("Enter a 5-digit number: ");
     scanf("%d", &num);
@@ -19,27 +19,29 @@ int main()
     temp /= 10;
 
     rem = temp % 10;
-    sum += rem * 1000;
+    sum += (rem * 1000);
     temp /= 10;
 
     rem = temp % 10;
-    sum += rem * 100;
+    sum += (rem * 100);
     temp /= 10;
 
     rem = temp % 10;
-    sum += rem * 10;
+    sum += (rem * 10);
     temp /= 10;
 
-    sum += sum;
+    sum += temp;
+    
+    printf("The reversed number is %d\n", sum);
 
-    if (sum == temp)
+    if (sum == num)
     {
-        printf("Original and reversed numbers are equal");
+        printf("Original and reversed number are equal");
     }
 
     else 
     {
-        printf("Original and reversed numbers are not equal");
+        printf("Original and reversed number are not equal");
     }
 
     return 0;
